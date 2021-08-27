ed44858f7ead8283a9e823fdcf665531e9775d7e
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2021-08-27 17:23:49 +0200

    chore(test): trying to test my ci again xD

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
-import { FC } from "react";
 
-const HomePage: FC = () => {
+const HomePage = ({ test }) => {
   return (
     <div>
+      {test}
       <h1>test</h1>
       <Link href="/about">about</Link>
       <p>Welcome to the index page!</p>
     </div>
-  );
-};
+  ); };
 
 export default HomePage;

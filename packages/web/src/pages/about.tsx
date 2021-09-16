95dba9bf60b323229f57d889a58b9dc510217fab
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2021-09-16 08:34:22 +0200

    test(chore): testing multiple commits part 4

--- packages/web/src/pages/about.tsx
+++ packages/web/src/pages/about.tsx
@@ -1,12 +1,16 @@
-import { FC } from "react";
 import Link from "next/link";
+import { FC } from "react";
+
+console.log(FC);
 
 const AboutPage: FC = () => {
   return (
     <div>
       <p>test</p>
       <Link href="/">index</Link>
-      <p>Welcome to the about page!</p> </div>);
+      <p>Welcome to the about page!</p>{" "}
+    </div>
+  );
 };
 
 export default AboutPage;

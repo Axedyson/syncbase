3b4b8a9ebba428ef53edfb013b09b0c0a91d5705
Thomas Madsen <thomas.madsen@sap.com>
2021-09-16 21:38:25 +0200

    test(chore): testing pr commit

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import { FC } from "react";
 
+console.log(FC);
+
 const HomePage: FC = () => {
   return (
     <div>
       <h1>test</h1>
       <Link href="/about">about</Link>
       <p>Welcome to the index page!</p>
-    </div>
-  );
-};
+    </div>); };
 
 export default HomePage;

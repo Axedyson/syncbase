0952302d95ca7dcc11a8f25f9d1fbc39b03d8b0d
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2021-11-29 23:12:05 +0100

    chore(eslint): 🔧 cleaned up some bit!

--- .eslintrc.js
+++ .eslintrc.js
@@ -175,7 +175,10 @@ module.exports = {
     },
     {
       files: ["*.graphql"],
-      extends: "plugin:@graphql-eslint/recommended",
+      extends: [
+        "plugin:@graphql-eslint/schema-recommended",
+        "plugin:@graphql-eslint/operations-recommended",
+      ],
       parserOptions: {
         operations: ["packages/web/src/graphql/**/*.graphql"],
         schema: "packages/server/schema.graphql",
@@ -192,23 +195,22 @@ module.exports = {
             arguments: ["Field", "Directive"],
           },
         ],
-        "@graphql-eslint/avoid-duplicate-fields": "error",
         "@graphql-eslint/unique-operation-name": "warn",
-        "@graphql-eslint/unique-fragment-name": "error",
-        // Disabling the default autofix feature for this rule. We don't want to fix it
-        // automatically since we need to change that in the actual typegraphql code!
-        "@graphql-eslint/no-case-insensitive-enum-values-duplicates": "off",
-        "no-autofix/@graphql-eslint/no-case-insensitive-enum-values-duplicates":
-          "warn",
+        "@graphql-eslint/unique-fragment-name": "warn",
         // Whenever vscode users get the "no-autofix/@graphql-eslint/no-unused-fields" rule error and they solve
         // the error immediately, they have to reload their entire window/eslint server
         // to make VS Code ESLint extension happy.
         // Because the user dev experience would be quite bad, the rule is disabled in the workspace settings
-        // Probably has something to do with: https://github.com/dotansimha/graphql-eslint/issues/593#issuecomment-956632777
+        // Probably has something to do with: https://github.com/dotansimha/graphql-eslint/issues/593#issuecomment-982066342
         // The same applies for these rules (there will probably be added more):
-        // "@graphql-eslint/fields-on-correct-type" and "@graphql-eslint/known-type-names"
-        // We are also disabling the autofix feature on the rule
+        // "@graphql-eslint/fields-on-correct-type", "@graphql-eslint/known-type-names", "@graphql-eslint/unique-operation-name",
+        // "@graphql-eslint/unique-fragment-name" and "@graphql-eslint/require-id-when-available"
+        // We are also disabling the autofix feature on the rule as we want to fix this error manually
+        // in the typegraphql typescript code!
         "no-autofix/@graphql-eslint/no-unused-fields": "error",
+        // We are also disabling the autofix feature on this rule for the same reason!
+        "@graphql-eslint/no-unreachable-types": "off",
+        "no-autofix/@graphql-eslint/no-unreachable-types": "error",
       },
       overrides: [
         {
@@ -216,6 +218,7 @@ module.exports = {
           rules: {
             "@graphql-eslint/strict-id-in-types": "off",
             "@graphql-eslint/executable-definitions": "off",
+            "@graphql-eslint/require-description": "off",
           },
         },
       ],

ba59f0a74574b5b10ae0761a3701368baaf648c7
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2022-03-15 20:21:53 +0100

    testing again!

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -16,7 +16,6 @@ import { IS_PROD, PORT, SESSION_NAME } from "./config/constants";
 import { UserResolver } from "./resolvers/user";
 import type { Context } from "./types";
 
-console.log("lol");
 (async () => {
   const orm = await MikroORM.init();
 

cbf86dbc94fbf6af6e3c45a367dd998b0f074d85
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2022-04-21 22:26:25 +0200

    chore(tests): 🔧 test stuff

--- packages/server/src/resolvers/__tests__/UserResolver.e2e.ts
+++ packages/server/src/resolvers/__tests__/UserResolver.e2e.ts
@@ -1,24 +1,17 @@
-import { gql } from "apollo-server-core";
-import { print } from "graphql/language/printer";
 import request from "supertest";
 import { redisClient } from "../../redis";
 import { startServer } from "../../server";
 import type { Connection, IDatabaseDriver, MikroORM } from "@mikro-orm/core";
-import type { DocumentNode } from "graphql";
 import type { Server } from "net";
 
 describe("User resolvers", () => {
   let server: Server;
   let orm: MikroORM<IDatabaseDriver<Connection>>;
-  let graphql: (query: DocumentNode) => request.Test;
+  let graphql: (query: string) => request.Test;
 
   beforeAll(async () => {
     ({ server, orm } = await startServer());
-    graphql = (query) =>
-      request
-        .agent(server)
-        .post("/graphql")
-        .send({ query: print(query) });
+    graphql = (query) => request(server).post("/graphql").send({ query });
   });
 
   afterAll(async () => {
@@ -28,7 +21,7 @@ describe("User resolvers", () => {
   });
 
   test("an example test that produces an error", () => {
-    const queryData = gql`
+    const queryData = /* GraphQL */ `
       query errorProducingGraphqlQuery {
         thisWillGenerateAnError
       }

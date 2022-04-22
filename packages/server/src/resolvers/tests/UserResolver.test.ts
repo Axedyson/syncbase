efdc1cf62d0dc3fa37593378ed4d645be0db6806
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2022-04-22 22:38:41 +0200

    test(ci): 🚨 👷 test clean up

--- packages/server/src/resolvers/tests/UserResolver.test.ts
+++ packages/server/src/resolvers/tests/UserResolver.test.ts
@@ -7,20 +7,24 @@ import type { Server } from "net";
 describe("User resolvers", () => {
   let server: Server;
   let orm: MikroORM<IDatabaseDriver<Connection>>;
+  let agent: request.SuperAgentTest;
   let graphql: (
+    agent: request.SuperAgentTest,
     query: string,
-    variables: Record<string, unknown>
+    variables?: Record<string, unknown>
   ) => request.Test;
 
   beforeAll(async () => {
     ({ server, orm } = await startServer());
     await orm.getSchemaGenerator().refreshDatabase();
-    graphql = (query, variables) =>
-      request(server)
+
+    agent = request.agent(server);
+    graphql = (agent, query, variables) =>
+      agent
         .post("/graphql")
-        .send({ query, variables })
+        .expect("Content-Type", /json/)
         .set("Accept", "application/json")
-        .expect("Content-Type", /json/);
+        .send({ query, variables });
   });
 
   afterAll(async () => {
@@ -30,7 +34,7 @@ describe("User resolvers", () => {
   });
 
   test("creating a user", () => {
-    const queryData = /* GraphQL */ `
+    const query = /* GraphQL */ `
       mutation RegisterUser($userInput: RegisterUserInput!) {
         registerUser(input: $userInput) {
           name
@@ -46,15 +50,35 @@ describe("User resolvers", () => {
       },
     };
 
-    return graphql(queryData, variables)
+    return graphql(agent, query, variables)
       .expect(200)
+      .expect(
+        "set-cookie",
+        /^ko=.+; Path=\/; Expires=.+; HttpOnly; SameSite=Lax$/
+      )
       .then((res) => {
         expect(res.body.data.registerUser.name).toBe("Bob");
       });
   });
 
+  test("me resolver", () => {
+    const query = /* GraphQL */ `
+      query Me {
+        me {
+          email
+        }
+      }
+    `;
+
+    return graphql(agent, query)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.data.me.email).toBe("anders@ewfwe.com");
+      });
+  });
+
   test("logging in a user", () => {
-    const queryData = /* GraphQL */ `
+    const query = /* GraphQL */ `
       mutation LoginUser($userInput: LoginUserInput!) {
         loginUser(input: $userInput) {
           email
@@ -69,15 +93,29 @@ describe("User resolvers", () => {
       },
     };
 
-    return graphql(queryData, variables)
+    return graphql(agent, query, variables)
       .expect(200)
       .then((res) => {
         expect(res.body.data.loginUser.email).toBe("anders@ewfwe.com");
       });
   });
 
+  test("logging user out", () => {
+    const query = /* GraphQL */ `
+      mutation LogoutUser {
+        logoutUser
+      }
+    `;
+
+    return graphql(agent, query)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.data.logoutUser).toBe(true);
+      });
+  });
+
   test("email already taken on sign up", () => {
-    const queryData = /* GraphQL */ `
+    const query = /* GraphQL */ `
       mutation RegisterUser($userInput: RegisterUserInput!) {
         registerUser(input: $userInput) {
           name
@@ -93,7 +131,7 @@ describe("User resolvers", () => {
       },
     };
 
-    return graphql(queryData, variables)
+    return graphql(agent, query, variables)
       .expect(200)
       .then((res) => {
         expect(res.body.errors[0].message).toBe("The email has been taken!");

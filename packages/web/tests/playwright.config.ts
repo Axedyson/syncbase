3af9e29f2c00938494c612bf337fce2e3b038feb
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2022-05-14 19:15:24 +0200

    ci(e2e): 👷 oh yes

--- packages/web/tests/playwright.config.ts
+++ packages/web/tests/playwright.config.ts
@@ -1,16 +1,26 @@
 import { devices } from "@playwright/test";
 import type { PlaywrightTestConfig } from "@playwright/test";
 
+// The code in this file is a bit finicky but it works, will update all of this when the next playwright update arrives!
 const webPort = "3002";
-const url = `http://localhost:${webPort}`;
-// Remember to run "yarn workspace @syncbase/server dev:e2e " or "... start:e2e" in another tab to start
+const url = `http://localhost:${process.env.CI ? "3000" : webPort}`;
+
+// Remember to run "yarn workspace @syncbase/server dev:e2e " in another tab to start
 // the server in playwright mode before running "yarn workspace @syncbase/web test". When the next
 // playwright update arrives we will be able to run multiple servers using the config options below
-// which will provide much better DX! And when that finally happens you can remove the "dev:e2e" and
-// "start:e2e" scripts in the /server/package.json file!
+// which will provide much better DX! And when that finally happens you can remove the "dev:e2e"
+// script in the /server/package.json file!
+
 const command = `yarn workspace @syncbase/web ${
   process.env.CI ? "start" : "dev"
 }`;
+
+const env = process.env.CI
+  ? undefined
+  : {
+      NEXT_PUBLIC_SERVER_PORT: "8082",
+      PORT: webPort,
+    };
 
 export default {
   testMatch: /\/tests\/.+\.test\.ts$/,
@@ -20,10 +30,7 @@ export default {
   webServer: {
     command,
     url,
-    env: {
-      NEXT_PUBLIC_SERVER_PORT: "8082",
-      PORT: webPort,
-    },
+    env,
   },
   use: {
     baseURL: url,

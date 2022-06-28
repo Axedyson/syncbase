e56996e20b2746f1b842817dd3776f9119ab1beb
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2022-06-28 07:36:22 +0200

    setting up terraform

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.21.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  # Configuration options
+}

5de0179911dd94e0d039115571ba6709341a2d96
Axedyson <28491857+Axedyson@users.noreply.github.com>
2022-08-30 15:20:17 +0200

    deploying for the second time, again 🐋

--- packages/server/src/middleware/sessionMiddleware.ts
+++ packages/server/src/middleware/sessionMiddleware.ts
@@ -2,6 +2,7 @@ import connectRedis from "connect-redis";
 import session from "express-session";
 import { IS_PROD, SESSION_COOKIE_NAME } from "../config/constants";
 import { redisClient } from "../redis";
+// import type { RequestHandler } from "express-serve-static-core";
 
 const RedisStore = connectRedis(session);
 

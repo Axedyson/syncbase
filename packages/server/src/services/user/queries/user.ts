3f4c03777dddd5fa7ee149af9bb82baef14e388e
Axedyson <28491857+Axedyson@users.noreply.github.com>
2022-10-30 00:23:01 +0200

    This should work

--- packages/server/src/services/user/queries/user.ts
+++ packages/server/src/services/user/queries/user.ts
@@ -2,5 +2,6 @@ import { User } from "../../../entities/User";
 import type { Context } from "src/types";
 
 export const user = ({ em }: Context, channelId: string) => {
+  console.log("lol");
   return em.findOne(User, { channelId });
 };
